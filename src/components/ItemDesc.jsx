f81032a176a1874db281e0cdb88d69fcc086549b
Caleb Barzee <barzeec@gmail.com>
2022-10-08 11:52:35 -0600

    product desc

---
+++ src/components/ItemDesc.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProductUpload from '../pages/ProductUpload';
+
+function ItemDesc() {
+    return (
+        <div class='Product-Description'>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
+}
+
+export default ItemDesc;
